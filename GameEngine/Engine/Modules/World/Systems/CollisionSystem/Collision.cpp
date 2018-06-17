5a37b7196cb866847b11688c2b4e5b40916ce2de
Paweł Głomski <pavel.glomski@gmail.com>
2018-06-17 10:33:21 +0200

    Add polygonShape::setVertices for automatic normals calculation

--- GameEngine/Engine/Modules/World/Systems/CollisionSystem/Collision.cpp
+++ GameEngine/Engine/Modules/World/Systems/CollisionSystem/Collision.cpp
@@ -1,6 +1,45 @@
 #include "Collision.h"
 
-void Polygon::setVertices(const Vec2f * newVertices, size_t count)
-{
 
+
+void Polygon::setVertices(const Vec2f * newVertices, size_t inputCount)
+{
+	if (!count)
+		return;
+
+	count = minValue(inputCount, MaxVerticesCount);
+	memcpy(vertices, newVertices, count * sizeof(Vec2f));
+	for (int i = 0; i < count; i++)
+	{
+		Vec2f vec = (vertices[(i + 1) % count] - vertices[i]);
+		vec.normalize();
+		normals[i] = { vec.y, -vec.x };
+	}
+}
+
+ShapeProjection Polygon::projectedOnNormal(const Vec2f& normal) const
+{
+	float leftVal = 0.f, rightVal = 0.f;
+	size_t leftIdx = 0, rightIdx = 0;
+
+	for (size_t i = 0; i < count; ++i)
+	{
+		float projMagnitude = dotProduct(normal, vertices[i]);
+		if (leftVal > projMagnitude)
+		{
+			leftVal = projMagnitude;
+			leftIdx = i;
+		}
+		else if (rightVal > projMagnitude)
+		{
+			rightVal = projMagnitude;
+			rightIdx = i;
+		}
+	}
+	return { leftIdx, rightIdx };
+}
+
+Collision Polygon::collide(const Polygon & other) const
+{
+	
 }
--- GameEngine/Engine/Modules/World/Systems/CollisionSystem/Collision.h
+++ GameEngine/Engine/Modules/World/Systems/CollisionSystem/Collision.h
@@ -8,20 +8,44 @@ struct HalfSpace
 };
 
 
-class Polygon
+struct Collision
 {
-public:
+	Vec2f n;
+	Vec2f contact_points[2] = {};
+	float depths[2] = {};
+	size_t count = 0;
+};
 
 
+struct PointProjection
+{
+	size_t index = 0;
+	float magnitude = 0.f;
+};
+
+struct ShapeProjection
+{
+	PointProjection left;
+	PointProjection right;
+};
+
+
+struct Polygon
+{
 public:
 	
 	void setVertices(const Vec2f* newVertices, size_t count);
+
+
+	ShapeProjection projectedOnNormal(const Vec2f& normal) const;
+
+	Collision collide(const Polygon& other) const;
 
 public:
 
 	static const size_t MaxVerticesCount = 8u;
 
-private:
+public:
 
 	size_t count = 0;
 
@@ -31,10 +55,3 @@ private:
 
 };
 
-struct Collision
-{
-	Vec2f n;
-	Vec2f contact_points[2] = {};
-	float depths[2] = {};
-	size_t count = 0;
-};
